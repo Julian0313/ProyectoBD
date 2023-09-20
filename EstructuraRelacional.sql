cf3f48d8893c8d550200f1ec3c6c0395955b1ede
julian0313 <davidgarzonlozano19@outlook.com>
2023-09-20 11:37:26 -0500

    .

--- EstructuraRelacional.sql
+++ EstructuraRelacional.sql
@@ -5,7 +5,7 @@ USE TiendaBdd
 GO
 
 CREATE TABLE estado(
-	id_estado BIT PRIMARY KEY
+	id_estado INT PRIMARY KEY
 	,nombre VARCHAR(50) NOT NULL
 	);
 
@@ -31,54 +31,54 @@ CREATE TABLE permiso(
 CREATE TABLE rol(
 	id_rol INT IDENTITY(1, 1) PRIMARY KEY
 	,nombre VARCHAR(50) NOT NULL
-	,fk_id_estado BIT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
+	,fk_id_estado INT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
 	);
 
 CREATE TABLE panel(
 	id_panel INT IDENTITY(1, 1) PRIMARY KEY
 	,nombre VARCHAR(100) NOT NULL
 	,fk_id_modulo INT FOREIGN KEY (fk_id_modulo) REFERENCES modulo(id_modulo)
-	,fk_id_estado BIT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
+	,fk_id_estado INT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
     );
 
-CREATE TABLE permisos_panel(
+CREATE TABLE permiso_panel(
 	id_permisos_panel INT IDENTITY(1, 1) PRIMARY KEY
 	,fk_id_rol INT FOREIGN KEY (fk_id_rol) REFERENCES rol(id_rol)
 	,fk_id_modulo INT FOREIGN KEY (fk_id_modulo) REFERENCES modulo(id_modulo)
 	,fk_id_panel INT FOREIGN KEY (fk_id_panel) REFERENCES panel(id_panel)
-	,fk_id_estado BIT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
-	,fk_id_accion BIT FOREIGN KEY (fk_id_accion) REFERENCES accion(id_accion)
+	,fk_id_estado INT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
+	,fk_id_accion INT FOREIGN KEY (fk_id_accion) REFERENCES accion(id_accion)
     );
 
-CREATE TABLE permisos_rol(
+CREATE TABLE permiso_rol(
 	id_rol_permiso INT IDENTITY(1, 1) PRIMARY KEY
 	,fk_id_rol INT FOREIGN KEY (fk_id_rol) REFERENCES rol(id_rol)
 	,fk_id_permiso INT FOREIGN KEY (fk_id_permiso) REFERENCES permisos(id_permiso)
 	,fk_id_modulo INT FOREIGN KEY (fk_id_modulo) REFERENCES modulo(id_modulo)
-	,fk_id_accion BIT FOREIGN KEY (fk_id_accion) REFERENCES accion(id_accion)
+	,fk_id_accion INT FOREIGN KEY (fk_id_accion) REFERENCES accion(id_accion)
 	);
 
-CREATE TABLE permisos_ususario(	
+CREATE TABLE permiso_ususario(	
 	id_permisos_ususario INT IDENTITY(1, 1) PRIMARY KEY
 	,fk_id_modulo INT FOREIGN KEY (fk_id_modulo) REFERENCES modulo(id_modulo)
 	,fk_id_permiso INT FOREIGN KEY (fk_id_permiso) REFERENCES permisos(id_permiso)
-	,fk_id_estado BIT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
-    ,fk_id_accion BIT FOREIGN KEY (fk_id_accion) REFERENCES accion(id_accion)
+	,fk_id_estado INT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
+    ,fk_id_accion INT FOREIGN KEY (fk_id_accion) REFERENCES accion(id_accion)
     );
 
 CREATE TABLE usuario (
     id_usuario INT IDENTITY(1, 1) PRIMARY KEY
     ,usuario VARCHAR(50) NOT NULL
     ,contrasena VARCHAR(50) NOT NULL
-	,segundo_factor BIT NOT NULL
-	,mostrar_segundo_fa BIT NOT NULL
+	,segundo_factor INT NOT NULL
+	,mostrar_segundo_fa INT NOT NULL
 	,intento INT NOT NULL
-	,bloqueo BIT NOT NULL
+	,bloqueo INT NOT NULL
 	,fecha_bloqueo DATETIME NOT NULL
     ,fecha_creacion DATETIME NOT NULL
     ,fecha_modificacion DATETIME NULL
     ,fk_id_rol INT FOREIGN KEY (fk_id_rol) REFERENCES rol(id_rol)
-    ,fk_id_estado BIT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
+    ,fk_id_estado INT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
     );
 
 CREATE TABLE historico_contrasena(
@@ -100,14 +100,14 @@ CREATE TABLE persona (
     ,celular VARCHAR(20) NOT NULL
     ,fecha_creacion DATETIME NULL
     ,fecha_modificacion DATETIME NULL
-    ,fk_id_estado BIT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
+    ,fk_id_estado INT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
 	,fk_id_usuario INT FOREIGN KEY (fk_id_usuario) REFERENCES usuario(id_usuario)
     );
 
 CREATE TABLE auditoria (
     id_auditoria INT IDENTITY(1,1) PRIMARY KEY
     ,fecha_evento DATETIME NOT NULL
-    ,fk_id_accion BIT FOREIGN KEY (fk_id_accion) REFERENCES accion(id_accion)
+    ,fk_id_accion INT FOREIGN KEY (fk_id_accion) REFERENCES accion(id_accion)
     ,nombre_tabla VARCHAR(100)
     ,id_resgustro_afectado INT
     ,fk_id_usuario INT FOREIGN KEY (fk_id_usuario) REFERENCES usuario(id_usuario)
