b8400e5e647dd511c11f882fcf05fb63caee7e56
julian0313 <davidgarzonlozano19@outlook.com>
2023-09-22 14:57:28 -0500

    poblar tablas y normalizacin bd

--- EstructuraRelacional.sql
+++ EstructuraRelacional.sql
@@ -1,77 +1,84 @@
-CREATE DATABASE TiendaBdd
+CREATE DATABASE Prueba
 GO
 
-USE TiendaBdd
+USE Prueba
 GO
 
-CREATE TABLE estado(
-	id_estado INT IDENTITY(1, 1) PRIMARY KEY
-	,nombre VARCHAR(50) NOT NULL
+CREATE TABLE tb_estado(
+	id_estado INT PRIMARY KEY
+	,descripcion VARCHAR(10) NOT NULL
+	);
+CREATE INDEX IX_estado_descripcion ON tb_estado(descripcion);
+
+CREATE TABLE tb_rol(
+	id_rol INT IDENTITY(1, 1) PRIMARY KEY
+    ,fk_id_estado INT FOREIGN KEY (fk_id_estado) REFERENCES tb_estado(id_estado)
+    ,abreviatura VARCHAR(5) NOT NULL
+	,descripcion VARCHAR(50) NOT NULL	
 	);
 
-CREATE TABLE modulo (
+CREATE TABLE tb_modulo (
     id_modulo INT IDENTITY(1, 1) PRIMARY KEY
-    ,abreviatura VARCHAR(50) NOT NULL
-    ,nombre VARCHAR(50) NOT NULL
+    ,fk_id_estado INT FOREIGN KEY (fk_id_estado) REFERENCES tb_estado(id_estado)
+    ,abreviatura VARCHAR(5) NOT NULL
+    ,descripcion VARCHAR(30) NOT NULL
     );
+CREATE INDEX IX_modulo_abreviatura ONtb_ tb_modulo(abreviatura);
 
-CREATE TABLE accion(
-	id_accion INT IDENTITY(1, 1) PRIMARY KEY
-	,nombre VARCHAR(50) NOT NULL
+CREATE TABLE tb_permiso(
+	id_permiso INT IDENTITY(1, 1) PRIMARY KEY
+	,nombre VARCHAR(30) NOT NULL
 	,descripcion VARCHAR(MAX) NOT NULL
     );
+CREATE INDEX IX_permiso_nombre ON tb_permiso(nombre);
 
-CREATE TABLE permiso(
-	id_permiso INT IDENTITY(1, 1) PRIMARY KEY
-	,nombre VARCHAR(50) NOT NULL
-	,fk_id_modulo INT FOREIGN KEY (fk_id_modulo) REFERENCES modulo(id_modulo)
-    ,fk_id_accion INT FOREIGN KEY (fk_id_accion) REFERENCES accion(id_accion)
-    ,fk_id_estado INT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
+CREATE TABLE tb_permiso_modulo(
+	id_permiso_modulo INT IDENTITY(1, 1) PRIMARY KEY
+    ,fk_id_rol INT FOREIGN KEY (fk_id_rol) REFERENCES tb_rol(id_rol)
+	,fk_id_modulo INT FOREIGN KEY (fk_id_modulo) REFERENCES tb_modulo(id_modulo)
+    ,fk_id_permiso INT FOREIGN KEY (fk_id_permiso) REFERENCES tb_permiso(id_permiso)
+    ,fk_id_estado INT FOREIGN KEY (fk_id_estado) REFERENCES tb_estado(id_estado)
 	);
-
-CREATE TABLE rol(
-	id_rol INT IDENTITY(1, 1) PRIMARY KEY
-	,nombre VARCHAR(50) NOT NULL
-	,fk_id_estado INT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
-	);
+CREATE INDEX IX_permiso_modulo_modulo ON tb_permiso_modulo(fk_id_modulo);
+CREATE INDEX IX_permiso_modulo_permiso ON tb_permiso_modulo(fk_id_permiso);
+CREATE INDEX IX_permiso_modulo_estado ON tb_permiso_modulo(fk_id_estado);
+CREATE INDEX IX_modulo_abreviatura ON tb_rol(abreviatura);
 
 CREATE TABLE panel(
 	id_panel INT IDENTITY(1, 1) PRIMARY KEY
-	,nombre VARCHAR(100) NOT NULL
+	,fk_id_modulo INT FOREIGN KEY (fk_id_modulo) REFERENCES modulo(id_modulo)
+	,fk_id_estado INT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
+	,nombre VARCHAR(50) NOT NULL
     ,habilitado INT NOT NULL
-	,fk_id_modulo INT FOREIGN KEY (fk_id_modulo) REFERENCES modulo(id_modulo)
-	,fk_id_estado INT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
     );
-
-CREATE TABLE permiso_panel(
-	id_permiso_panel INT IDENTITY(1, 1) PRIMARY KEY
-	,fk_id_rol INT FOREIGN KEY (fk_id_rol) REFERENCES rol(id_rol)
-	,fk_id_modulo INT FOREIGN KEY (fk_id_modulo) REFERENCES modulo(id_modulo)
-	,fk_id_panel INT FOREIGN KEY (fk_id_panel) REFERENCES panel(id_panel)
-	,fk_id_estado INT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
-	,fk_id_accion INT FOREIGN KEY (fk_id_accion) REFERENCES accion(id_accion)
-    );
+CREATE INDEX IX_panel_modulo ON panel(fk_id_modulo);
+CREATE INDEX IX_panel_estado ON panel(fk_id_estado);
 
 CREATE TABLE permiso_rol(
 	id_rol_permiso INT IDENTITY(1, 1) PRIMARY KEY
 	,fk_id_rol INT FOREIGN KEY (fk_id_rol) REFERENCES rol(id_rol)
 	,fk_id_permiso INT FOREIGN KEY (fk_id_permiso) REFERENCES permiso(id_permiso)
-	,fk_id_modulo INT FOREIGN KEY (fk_id_modulo) REFERENCES modulo(id_modulo)
-	,fk_id_accion INT FOREIGN KEY (fk_id_accion) REFERENCES accion(id_accion)
+    ,fk_id_estado INT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
 	);
 
-CREATE TABLE permiso_ususario(	
-	id_permiso_ususario INT IDENTITY(1, 1) PRIMARY KEY
-	,fk_id_modulo INT FOREIGN KEY (fk_id_modulo) REFERENCES modulo(id_modulo)
-	,fk_id_permiso INT FOREIGN KEY (fk_id_permiso) REFERENCES permiso(id_permiso)
+CREATE TABLE permiso_panel(
+	id_permiso_panel INT IDENTITY(1, 1) PRIMARY KEY
+	,fk_id_rol INT FOREIGN KEY (fk_id_rol) REFERENCES rol(id_rol)
+	,fk_id_panel INT FOREIGN KEY (fk_id_panel) REFERENCES panel(id_panel)
 	,fk_id_estado INT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
-    ,fk_id_accion INT FOREIGN KEY (fk_id_accion) REFERENCES accion(id_accion)
+	,fk_id_permiso INT FOREIGN KEY (fk_id_permiso) REFERENCES permiso(id_permiso)
     );
+CREATE INDEX IX_permiso_panel_rol ON permiso_panel(fk_id_rol);
+CREATE INDEX IX_permiso_panel_panel ON permiso_panel(fk_id_panel);
+CREATE INDEX IX_permiso_panel_estado ON permiso_panel(fk_id_estado);
+CREATE INDEX IX_permiso_panel_permiso ON permiso_panel(fk_id_permiso);
 
 CREATE TABLE usuario (
     id_usuario INT IDENTITY(1, 1) PRIMARY KEY
+    ,fk_id_rol INT FOREIGN KEY (fk_id_rol) REFERENCES rol(id_rol)
+    ,fk_id_estado INT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
     ,usuario VARCHAR(100) NOT NULL
-    ,contrasena VARCHAR(50) NOT NULL
+    ,contrasena VARCHAR(30) NOT NULL
 	,segundo_factor BIT NOT NULL
 	,mostrar_segundo_fa BIT NOT NULL
 	,intento INT NOT NULL
@@ -79,42 +86,48 @@ CREATE TABLE usuario (
 	,fecha_bloqueo DATETIME NULL
     ,fecha_creacion DATETIME NOT NULL
     ,fecha_modificacion DATETIME NULL
-    ,fk_id_rol INT FOREIGN KEY (fk_id_rol) REFERENCES rol(id_rol)
-    ,fk_id_estado INT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
     );
-
-CREATE TABLE historico_contrasena(
-	id_historico INT IDENTITY(1, 1) PRIMARY KEY
-	,contrasena VARCHAR(50) NOT NULL
-	,fecha DATETIME NULL
-	,fk_id_usuario INT FOREIGN KEY (fk_id_usuario) REFERENCES usuario(id_usuario)
-    );
+CREATE INDEX IX_usuario_rol ON usuario(fk_id_rol);
+CREATE INDEX IX_usuario_estado ON usuario(fk_id_estado);
 
 CREATE TABLE persona (
     id_persona INT IDENTITY(1, 1) PRIMARY KEY
-    ,identificacion VARCHAR(15) NOT NULL
-    ,primer_nombre VARCHAR(20) NOT NULL
-    ,segundo_nombre VARCHAR(20)
-    ,primer_apellido VARCHAR(20) NOT NULL
-    ,segundo_apellido VARCHAR(20)
-    ,email VARCHAR(100) NOT NULL
-    ,direccion VARCHAR(100) NOT NULL
-    ,celular VARCHAR(20) NOT NULL
-    ,fecha_creacion DATETIME NULL
-    ,fecha_modificacion DATETIME NULL
     ,fk_id_estado INT FOREIGN KEY (fk_id_estado) REFERENCES estado(id_estado)
 	,fk_id_usuario INT FOREIGN KEY (fk_id_usuario) REFERENCES usuario(id_usuario)
+    ,identificacion VARCHAR(15) NOT NULL
+    ,primer_nombre VARCHAR(30) NOT NULL
+    ,segundo_nombre VARCHAR(30)
+    ,primer_apellido VARCHAR(30) NOT NULL
+    ,segundo_apellido VARCHAR(30)
+    ,email VARCHAR(100) NOT NULL
+    ,direccion VARCHAR(100) NOT NULL
+    ,numero_contacto VARCHAR(10) NOT NULL
+    ,numero_contacto2 VARCHAR(10)  NULL
+    ,fecha_creacion DATETIME NOT NULL
+    ,fecha_modificacion DATETIME NULL
     );
+CREATE INDEX IX_persona_estado ON persona(fk_id_estado);
+CREATE INDEX IX_persona_usuario ON persona(fk_id_usuario);
+
+CREATE TABLE historico_contrasena(
+	id_historico INT IDENTITY(1, 1) PRIMARY KEY
+	,fk_id_usuario INT FOREIGN KEY (fk_id_usuario) REFERENCES usuario(id_usuario)
+	,contrasena VARCHAR(50) NOT NULL
+	,fecha DATETIME NULL
+    );
+CREATE INDEX IX_historico_contrasena_usuario ON historico_contrasena(fk_id_usuario);
 
 CREATE TABLE auditoria (
     id_auditoria INT IDENTITY(1,1) PRIMARY KEY
+    ,fk_id_permiso INT FOREIGN KEY (fk_id_permiso) REFERENCES permiso(id_permiso)
+    ,fk_id_usuario INT FOREIGN KEY (fk_id_usuario) REFERENCES usuario(id_usuario)
     ,fecha_evento DATETIME NOT NULL
-    ,fk_id_accion INT FOREIGN KEY (fk_id_accion) REFERENCES accion(id_accion)
     ,nombre_tabla VARCHAR(100)
     ,id_resgustro_afectado INT
-    ,fk_id_usuario INT FOREIGN KEY (fk_id_usuario) REFERENCES usuario(id_usuario)
-    ,detalles VARCHAR(MAX)
+    ,detalles VARCHAR(MAX)   
     );
+CREATE INDEX IX_auditoria_permiso ON auditoria(fk_id_permiso);
+CREATE INDEX IX_auditoria_usuario ON auditoria(fk_id_usuario);
 
 CREATE TABLE historial_acceso_usuario (
     id_historial_acceso INT IDENTITY(1,1) PRIMARY KEY
@@ -123,6 +136,7 @@ CREATE TABLE historial_acceso_usuario (
     ,direccion_ip VARCHAR(15)
     ,informacion_dispositivo VARCHAR(200)
     );
+CREATE INDEX IX_historial_acceso_usuario ON historial_acceso_usuario(fk_id_usuario);
 
 CREATE TABLE token (
     id_token INT IDENTITY(1, 1) PRIMARY KEY,
@@ -132,6 +146,7 @@ CREATE TABLE token (
     fecha_creacion DATETIME NOT NULL,
     fecha_expiracion DATETIME NOT NULL
     );
+CREATE INDEX IX_token_usuario ON token(fk_id_usuario);
 
 CREATE TABLE sesion (
     id_sesion INT IDENTITY(1, 1) PRIMARY KEY
@@ -140,4 +155,5 @@ CREATE TABLE sesion (
     ,fecha_creacion DATETIME NOT NULL
     ,fecha_expiracion DATETIME NOT NULL
     );
+CREATE INDEX IX_sesion_usuario ON sesion(fk_id_usuario);
 
--- Inserts.sql
+++ Inserts.sql
@@ -1,23 +1,52 @@
-USE TiendaBdd
+USE Prueba
 GO
-INSERT INTO rol
-		(nombre)
-		VALUES 
-		('Admin'),
-		('Clie'),
-		('Empl')
-GO
-INSERT INTO modulo
-		(abreviatura, nombre)
-		VALUES
-		('USU','Usuario'),
-		('PER','Permisos')
-GO
-INSERT INTO accion
-		(nombre, descripcion)
-		VALUES
-		('Acceso','Permite acceso al modulo'),
-		('Crear','Permite crear nuevos registros'),
-		('Leer','Permite consultar registros'),
-		('Editar','Permite editar registros existentes'),
-		('Eliminar','Permite eliminar registros existentes')
+INSERT INTO estado (id_estado, descripcion) VALUES 
+    (1, 'Activo'),
+    (0, 'Inactivo');
+
+INSERT INTO modulo (fk_id_estado, abreviatura, descripcion) VALUES 
+	(1, 'CFG', 'Modulo de configuracion'),
+	(1, 'RTS', 'Modulo de reportes');
+
+INSERT INTO permiso (nombre, descripcion) VALUES
+	('Acceso','Permite acceso al modulo o peneles'),
+	('Crear','Permite crear nuevos registros'),
+	('Leer','Permite consultar registros'),
+	('Editar','Permite editar registros existentes'),
+	('Eliminar','Permite eliminar registros existentes');
+
+INSERT INTO permiso_modulo (fk_id_rol, fk_id_modulo, fk_id_permiso, fk_id_estado) VALUES
+	(1, 1, 1, 1),
+	(2, 2, 1, 1);
+
+INSERT INTO rol (fk_id_estado, abreviatura, descripcion) VALUES 
+    (1, 'ADMIN', 'Rol de administrador'),
+    (1, 'USER', 'Rol de usuario');
+
+INSERT INTO panel (fk_id_modulo, fk_id_estado, nombre, habilitado) VALUES 
+	(1, 1, 'SEGURIDAD', 1),
+	(1, 1, 'REPORTES', 1),
+	(2, 1, 'REPORTES', 1);
+
+INSERT INTO permiso_rol (fk_id_rol, fk_id_permiso, fk_id_estado) VALUES
+	(1, 2, 1),
+	(1, 3, 1),
+	(1, 4, 1),
+	(1, 5, 1),
+	(2, 2, 1),
+	(2, 3, 1),
+	(2, 4, 1),
+	(2, 5, 0);
+
+INSERT INTO permiso_panel (fk_id_rol, fk_id_panel,  fk_id_estado, fk_id_permiso) VALUES
+	(1, 1, 1, 1),
+	(2, 2, 1, 1),
+	(2, 1, 0, 1);
+
+INSERT INTO usuario (fk_id_rol, fk_id_estado, usuario, contrasena, segundo_factor, mostrar_segundo_fa, intento, bloqueo, fecha_bloqueo, fecha_creacion, fecha_modificacion) VALUES
+    (1, 1, 'admin@gmail.com', 'adminpassword123', 1, 0, 3, 0, NULL, GETDATE(), NULL),
+    (2, 1, 'jane.smith@example.com', 'password123', 1, 0, 3, 0, NULL, GETDATE(), NULL);
+
+INSERT INTO persona (fk_id_estado, fk_id_usuario, identificacion, primer_nombre, primer_apellido, email, direccion, numero_contacto, fecha_creacion) VALUES
+    (1, 1, '123456789', 'John', 'Doe', 'admin@gmail.com', 'Calle falsa 123', '123456789', GETDATE()),
+    (1, 2, '987654321', 'Jane', 'Smith', 'jane.smith@example.com', 'Cra falsa 1234', '987654321', GETDATE());
