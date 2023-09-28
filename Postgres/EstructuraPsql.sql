27eea57b3108971312fdbbe611f7925d67879b62
julian0313 <davidgarzonlozano19@outlook.com>
2023-09-28 10:16:08 -0500

    Creacion de index Psql

--- Postgres/EstructuraPsql.sql
+++ Postgres/EstructuraPsql.sql
@@ -2,6 +2,8 @@ CREATE TABLE estado (
     id_estado SERIAL PRIMARY KEY,
     descripcion VARCHAR(10) NOT NULL
 );
+CREATE INDEX IX_estado_id ON estado(id_estado);
+CREATE INDEX IX_estado_descripcion ON estado(descripcion);
 
 CREATE TABLE rol (
     id_rol SERIAL PRIMARY KEY,
@@ -9,6 +11,7 @@ CREATE TABLE rol (
     abreviatura VARCHAR(5) NOT NULL,
     descripcion VARCHAR(50) NOT NULL
 );
+CREATE INDEX IX_rol_fk_id_estado ON rol(fk_id_estado);
 
 CREATE TABLE modulo (
     id_modulo SERIAL PRIMARY KEY,
@@ -16,6 +19,7 @@ CREATE TABLE modulo (
     abreviatura VARCHAR(5) NOT NULL,
     descripcion VARCHAR(30) NOT NULL
 );
+CREATE INDEX IX_modulo_fk_id_estado ON modulo(fk_id_estado);
 
 CREATE TABLE permiso (
     id_permiso SERIAL PRIMARY KEY,
@@ -30,6 +34,7 @@ CREATE TABLE rol_permiso_modulo (
     fk_id_permiso INT REFERENCES permiso(id_permiso),
     fk_id_estado INT REFERENCES estado(id_estado)
 );
+CREATE INDEX IX_permiso_modulo_composite ON rol_permiso_modulo(fk_id_rol, fk_id_modulo, fk_id_permiso, fk_id_estado);
 
 CREATE TABLE panel (
     id_panel SERIAL PRIMARY KEY,
@@ -42,6 +47,7 @@ CREATE TABLE panel (
     ruta TEXT NOT NULL,
     habilitado INT NOT NULL
 );
+CREATE INDEX IX_panel_composite ON panel(fk_id_modulo, fk_id_estado);
 
 CREATE TABLE rol_permiso_panel (
     id_permiso_panel SERIAL PRIMARY KEY,
@@ -50,6 +56,7 @@ CREATE TABLE rol_permiso_panel (
     fk_id_permiso INT REFERENCES permiso(id_permiso),
     fk_id_estado INT REFERENCES estado(id_estado)
 );
+CREATE INDEX IX_permiso_panel_composite ON rol_permiso_panel(fk_id_rol, fk_id_panel, fk_id_estado, fk_id_permiso);
 
 CREATE TABLE usuario (
     id_usuario SERIAL PRIMARY KEY,
@@ -65,6 +72,16 @@ CREATE TABLE usuario (
     fecha_creacion TIMESTAMP NOT NULL,
     fecha_modificacion TIMESTAMP
 );
+CREATE INDEX IX_usuario_fk_id_rol ON usuario(fk_id_rol);
+CREATE INDEX IX_usuario_fk_id_estado ON usuario(fk_id_estado);
+
+CREATE TABLE permiso_usuario (
+    id_permiso_usuario SERIAL PRIMARY,
+    fk_id_usuario INT REFERENCES usuario(id_usuario),
+    fk_id_permiso INT REFERENCES permiso(id_permiso),
+    fk_id_estado INT REFERENCES estado(id_estado),
+);
+CREATE INDEX IX_permiso_usuario_composite ON permiso_usuario(id_usuario, fk_id_estado, fk_id_permiso);
 
 CREATE TABLE persona (
     id_persona SERIAL PRIMARY KEY,
@@ -82,6 +99,8 @@ CREATE TABLE persona (
     fecha_creacion TIMESTAMP NOT NULL,
     fecha_modificacion TIMESTAMP
 );
+CREATE INDEX IX_persona_fk_id_estado ON persona(fk_id_estado);
+CREATE INDEX IX_persona_fk_id_usuario ON persona(fk_id_usuario);
 
 CREATE TABLE historico_contrasena (
     id_historico SERIAL PRIMARY KEY,
@@ -89,6 +108,7 @@ CREATE TABLE historico_contrasena (
     contrasena VARCHAR(50) NOT NULL,
     fecha TIMESTAMP
 );
+CREATE INDEX IX_historico_contrasena_fk_id_usuario ON historico_contrasena(fk_id_usuario);
 
 CREATE TABLE auditoria_usuario (
     id_auditoria_usuario SERIAL PRIMARY KEY,
@@ -98,6 +118,8 @@ CREATE TABLE auditoria_usuario (
     id_resgistro_afectado INT,
     detalles TEXT
 );
+CREATE INDEX IX_auditoria_usuario_fk_id_permiso ON auditoria_usuario(fk_id_permiso);
+CREATE INDEX IX_auditoria_usuario_fk_id_usuario ON auditoria_usuario(fk_id_usuario);
 
 CREATE TABLE auditoria_permiso (
     id_auditoria_permiso SERIAL PRIMARY KEY,
@@ -108,6 +130,8 @@ CREATE TABLE auditoria_permiso (
     id_resgistro_afectado INT,
     detalles TEXT
 );
+CREATE INDEX IX_auditoria_Permiso_fk_id_permiso ON auditoria_Permiso(fk_id_permiso);
+CREATE INDEX IX_auditoria_Permiso_fk_id_usuario ON auditoria_Permiso(fk_id_usuario);
 
 CREATE TABLE historial_acceso_usuario (
     id_historial_acceso SERIAL PRIMARY KEY,
@@ -116,6 +140,7 @@ CREATE TABLE historial_acceso_usuario (
     direccion_ip VARCHAR(15),
     informacion_dispositivo VARCHAR(200)
 );
+CREATE INDEX IX_historial_acceso_usuario_fk_id_usuario ON historial_acceso_usuario(fk_id_usuario);
 
 CREATE TABLE token (
     id_token SERIAL PRIMARY KEY,
@@ -125,6 +150,7 @@ CREATE TABLE token (
     fecha_creacion TIMESTAMP NOT NULL,
     fecha_expiracion TIMESTAMP NOT NULL
 );
+CREATE INDEX IX_token_fk_id_usuario ON token(fk_id_usuario);
 
 CREATE TABLE sesion (
     id_sesion SERIAL PRIMARY KEY,
@@ -133,3 +159,4 @@ CREATE TABLE sesion (
     fecha_creacion TIMESTAMP NOT NULL,
     fecha_expiracion TIMESTAMP NOT NULL
 );
+CREATE INDEX IX_sesion_fk_id_usuario ON sesion(fk_id_usuario);
